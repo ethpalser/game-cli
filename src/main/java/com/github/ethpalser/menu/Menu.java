08132215d779890ea8f10f0d14527e98aef4d437
Ethan Palser <39565969+ethpalser@users.noreply.github.com>
2024-06-28 21:36:27 -0400

    Update Menu.java
    Update Menu from interface to abstract class, as this better defines an object.

--- src/main/java/com/github/ethpalser/menu/Menu.java
+++ src/main/java/com/github/ethpalser/menu/Menu.java
@@ -3,24 +3,89 @@ package com.github.ethpalser.menu;
 import com.github.ethpalser.menu.event.Event;
 import com.github.ethpalser.menu.event.Result;
 import java.util.Map;
+import java.util.TreeMap;
 
-public interface Menu {
+/**
+ * Menu objects are containers of data that can contain sub-menus (children). These objects capable of handling events,
+ * that it supports.
+ */
+public abstract class Menu {
 
-    String getName();
+    private final String name;
+    private final Map<String, Menu> children;
 
-    Result handleEvent(Event event);
+    Menu(final String name, final Menu[] children) {
+        this.name = name;
+        this.children = new TreeMap<>();
+        this.addChildren(children);
+    }
 
-    Map<String, Menu> getChildren();
+    /**
+     * Returns this menu's name.
+     *
+     * @return String representing its name
+     */
+    String getName() {
+        return this.name;
+    }
 
-    void addChildren(Menu... children);
+    /**
+     * Handles an event sent to this object. The result contains data that may be useful for the event emitter.
+     * It is recommended to return an empty Result instead of null for events that are not handled.
+     *
+     * @param event Event that is to be handled
+     * @return Result of the event
+     * @see Event
+     * @see Result
+     */
+    abstract Result handleEvent(Event event);
 
-    default void addChild(Menu child) {
+    /**
+     * Returns this Menu's children.
+     *
+     * @return Map of Menu name to Menu
+     */
+    Map<String, Menu> getChildren() {
+        return this.children;
+    }
+
+    /**
+     * Add one or more Menu objects.
+     *
+     * @param children array of Menu objects
+     */
+    void addChildren(Menu... children) {
+        for (Menu child : children) {
+            this.children.put(child.getName(), child);
+        }
+    }
+
+    /**
+     * Add a single Menu object.
+     *
+     * @param child Menu object
+     */
+    void addChild(Menu child) {
         addChildren(child);
     }
 
-    void removeChildren(String... children);
+    /**
+     * Remove up to one or more Menu objects, if this Menu contains a Menu with these names.
+     *
+     * @param names String array representing a list of Menu names
+     */
+    void removeChildren(String... names) {
+        for (String name : names) {
+            this.children.remove(name);
+        }
+    }
 
-    default void removeChild(String name) {
+    /**
+     * Remove up to one Menu object, if this Menu contains a Menu with that name
+     *
+     * @param name String representing a Menu's name
+     */
+    void removeChild(String name) {
         removeChildren(name);
     }
 
