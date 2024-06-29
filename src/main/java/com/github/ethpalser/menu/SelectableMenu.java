567c8872aec520733d46f81798d59543546fefc2
Ethan Palser <39565969+ethpalser@users.noreply.github.com>
2024-06-28 21:37:52 -0400

    Create SelectableMenu.java

---
+++ src/main/java/com/github/ethpalser/menu/SelectableMenu.java
@@ -0,0 +1,38 @@
+package com.github.ethpalser.menu;
+
+import com.github.ethpalser.menu.event.Result;
+import com.github.ethpalser.menu.event.Selectable;
+
+public class SelectableMenu<T> extends SimpleMenu<T> implements Selectable {
+
+    private boolean isAvailable;
+
+    public SelectableMenu(String name, boolean isVisible, T display, boolean isAvailable, Menu... children) {
+        super(name, isVisible, display, children);
+        this.isAvailable = isAvailable;
+    }
+
+    public SelectableMenu(String name, boolean isVisible, T display, boolean isAvailable) {
+        this(name, isVisible, display, isAvailable, (Menu[]) null);
+        this.isAvailable = true;
+    }
+
+    public SelectableMenu(String name, boolean isVisible, T display) {
+        this(name, isVisible, display, true);
+    }
+
+    public SelectableMenu(final String name, final Menu[] children) {
+        super(name, children);
+        this.isAvailable = false;
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    @Override
+    public Result onSelect() {
+        return null;
+    }
+}
