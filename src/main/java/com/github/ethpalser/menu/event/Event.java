d48bba39cbd10cc3008a377709ea0ea7869420ce
Ethan Palser <39565969+ethpalser@users.noreply.github.com>
2024-06-29 17:07:28 -0400

    Create Event.java

---
+++ src/main/java/com/github/ethpalser/menu/event/Event.java
@@ -0,0 +1,24 @@
+package com.github.ethpalser.menu.event;
+
+public class Event {
+
+    private final EventType eventType;
+    private final String commandString;
+
+    public Event(EventType eventType, String commandString) {
+        this.eventType = eventType;
+        this.commandString = commandString;
+    }
+
+    public Event(EventType eventType) {
+        this(eventType, null);
+    }
+
+    public EventType getEventType() {
+        return eventType;
+    }
+
+    public String getCommandString() {
+        return commandString;
+    }
+}
