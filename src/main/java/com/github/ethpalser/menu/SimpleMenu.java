e6a86af7125ce34e6be68e23630da6ec70205276
Ethan Palser <39565969+ethpalser@users.noreply.github.com>
2024-06-30 17:13:35 -0400

    Create SimpleMenu.java

---
+++ src/main/java/com/github/ethpalser/menu/SimpleMenu.java
@@ -0,0 +1,30 @@
+package com.github.ethpalser.menu;
+
+import com.github.ethpalser.util.StringUtils;
+
+public class SimpleMenu extends Menu {
+
+    public SimpleMenu(String name, MenuItem[] children) {
+        super(name, children);
+    }
+
+    public SimpleMenu(String name) {
+        this(name, new MenuItem[]{});
+    }
+
+    @Override
+    public String getTextDisplay() {
+        return this.buildTextDisplayFromChildren(this.getChildren().values().toArray(new MenuItem[0]));
+    }
+
+    private String buildTextDisplayFromChildren(MenuItem[] children) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < children.length; i++) {
+            if (i != 0)
+                sb.append("\n");
+            sb.append(i).append(". ").append(StringUtils.capitalizeWord(children[i].getName()));
+        }
+        return sb.toString();
+    }
+
+}
