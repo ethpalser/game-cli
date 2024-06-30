f2ef6e60188c90aa861109cfc4d9aac16848b054
Ethan Palser <39565969+ethpalser@users.noreply.github.com>
2024-06-29 21:18:49 -0400

    Create CommandMenuWriter.java

---
+++ src/main/java/com/github/ethpalser/ui/CommandMenuWriter.java
@@ -0,0 +1,29 @@
+package com.github.ethpalser.ui;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+
+public class CommandMenuWriter {
+
+    private final static String WRITER_CLOSED_ERROR_MESSAGE = "writer closed";
+
+    private final BufferedWriter bw;
+    private boolean canWrite;
+
+    public CommandMenuWriter(final BufferedWriter ioWriter) {
+        this.bw = ioWriter;
+    }
+
+    public void write(String message) throws IOException {
+        if (!canWrite) {
+            throw new IOException(WRITER_CLOSED_ERROR_MESSAGE);
+        }
+        this.bw.write(message);
+    }
+
+    public void close() throws IOException {
+        this.bw.close();
+        this.canWrite = false;
+    }
+
+}
