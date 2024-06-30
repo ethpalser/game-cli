63a553e6860000d656bd316d06e7f44d3f46938e
Ethan Palser <39565969+ethpalser@users.noreply.github.com>
2024-06-30 17:12:36 -0400

    Create StringUtils.java

---
+++ src/main/java/com/github/ethpalser/util/StringUtils.java
@@ -0,0 +1,18 @@
+package com.github.ethpalser.util;
+
+import java.util.Locale;
+
+public class StringUtils {
+
+    public static String capitalizeWord(String word) {
+        if (word == null) {
+            return null;
+        }
+        if (word.length() == 1) {
+            return word.toUpperCase(Locale.ROOT);
+        }
+        String lower = word.toLowerCase(Locale.ROOT);
+        return lower.substring(0, 1).toUpperCase(Locale.ROOT) + lower.substring(1);
+    }
+
+}
