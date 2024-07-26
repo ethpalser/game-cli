bb023cd2da00290bd7dde7180a48cba34672fe73
Ethan Palser <39565969+ethpalser@users.noreply.github.com>
2024-07-26 15:29:18 -0400

    Update Result.java
    Added static strings for common cases

--- src/main/java/com/ethpalser/cli/menu/event/Result.java
+++ src/main/java/com/ethpalser/cli/menu/event/Result.java
@@ -2,6 +2,11 @@ package com.ethpalser.cli.menu.event;
 
 public final class Result {
 
+    public static final String SUCCESS_MESSAGE = "event success";
+    public static final String MISSING_MESSAGE = "event ignored, as an event listener is missing";
+    public static final String INVALID_MESSAGE = "event failed, as null or illegal event provided";
+    public static final String ERROR_MESSAGE = "event failed, as an error occurred";
+
     private final String message;
     private final boolean hasError;
 
