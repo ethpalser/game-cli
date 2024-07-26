4b2b4519dd164890e6756b7ef3d4823191d9ce97
Ethan Palser <39565969+ethpalser@users.noreply.github.com>
2024-07-26 16:11:48 -0400

    Update Main.java
    Fixed method matching class name, and ConsoleRunner using a null context

--- src/main/java/com/ethpalser/cli/Main.java
+++ src/main/java/com/ethpalser/cli/Main.java
@@ -1,10 +1,11 @@
 package com.ethpalser.cli;
 
+import com.ethpalser.cli.menu.Context;
 import com.ethpalser.cli.menu.Menu;
 import com.ethpalser.cli.menu.SimpleMenu;
 import com.ethpalser.cli.console.ConsoleRunner;
 
-public class main {
+public class Main {
 
     public static void main(String[] args) {
         Menu main = new SimpleMenu("main");
@@ -12,7 +13,7 @@ public class main {
         main.addChild(new Menu("start"));
         main.addChild(new Menu("exit"));
 
-        ConsoleRunner menu = new ConsoleRunner(null);
+        ConsoleRunner menu = new ConsoleRunner(Context.getInstance());
         menu.open();
     }
 
