8e3afacb0fa904683029d9041e4697b12c450194
Ethan Palser <39565969+ethpalser@users.noreply.github.com>
2024-07-28 23:49:30 -0400

    Update ConsoleRunner.java
    Updated to use latest Context. Extracted single execution of event loop in open() to its own method called runCycle().

--- src/main/java/com/ethpalser/cli/console/ConsoleRunner.java
+++ src/main/java/com/ethpalser/cli/console/ConsoleRunner.java
@@ -6,17 +6,13 @@ import com.ethpalser.cli.menu.MenuItem;
 import com.ethpalser.cli.menu.event.Event;
 import com.ethpalser.cli.menu.event.EventType;
 import com.ethpalser.cli.menu.event.Result;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import com.ethpalser.cli.menu.exception.InvalidContextException;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.List;
 
 public class ConsoleRunner {
 
     private final Context context;
-    private Menu main;
 
     public ConsoleRunner(Context context) {
         if (context == null) {
@@ -27,16 +23,7 @@ public class ConsoleRunner {
 
     public ConsoleRunner(Context context, Menu main) {
         this(context);
-        this.main = main;
-        this.context.push(this.main);
-    }
-
-    private Menu getActive() {
-        return this.context.peek();
-    }
-
-    private void setActive(Menu menu) {
-        this.context.push(menu);
+        this.context.setDefault(main);
     }
 
     private String awaitInput(ConsoleReader reader) {
@@ -44,7 +31,7 @@ public class ConsoleRunner {
             return null;
         }
         try {
-            List<String> visibleOptions = this.getActive().getChildren().values()
+            List<String> visibleOptions = this.context.peek().getChildren().values()
                     .stream().filter(child -> !child.isHidden()).map(MenuItem::getName).toList();
             return reader.readOption(visibleOptions);
         } catch (IOException e) {
@@ -72,42 +59,68 @@ public class ConsoleRunner {
     }
 
     /**
-     * Establish open streams and run main loop.
+     * Perform one cycle of sending events and receiving input. This may be used to perform finer control over the
+     * runtime by specifying when a cycle should be performed. This method will never close the reader and writer
+     * provided to it, as such you should close these separately
+     *
+     * @param reader ConsoleReader that handles IO reads
+     * @param writer ConsoleWriter that handles IO writes
+     * @return boolean false if an escape is registered, otherwise true
+     */
+    public boolean runCycle(ConsoleReader reader, ConsoleWriter writer) throws IllegalArgumentException, IOException,
+            InvalidContextException {
+        if (reader == null || writer == null) {
+            throw new IllegalArgumentException("i/o objects are null");
+        }
+        if (!reader.ready() || !writer.ready()) {
+            throw new IOException("i/o streams closed");
+        }
+        Menu activeMenu = this.context.peek();
+        if (activeMenu == null) {
+            throw new InvalidContextException();
+        }
+
+        this.sendEvent(new Event(EventType.PRE_RENDER), activeMenu);
+        this.awaitOutput(writer, activeMenu.getTextDisplay());
+        this.sendEvent(new Event(EventType.RENDER), activeMenu);
+        this.sendEvent(new Event(EventType.POST_RENDER), activeMenu);
+
+        String input = this.awaitInput(reader);
+        if (input == null || reader.getEscapeCommands().contains(input)) {
+            return false;
+        }
+
+        if (reader.getBackCommands().contains(input)) {
+            this.context.pop();
+        } else {
+            // Split the input, as it may have arguments and would not match a menu option
+            String[] split = input.split("\\s+");
+            MenuItem selected = activeMenu.getChild(split[0]);
+            if (selected != null && !selected.isDisabled()) {
+                this.sendEvent(new Event(EventType.EXECUTE, input), selected);
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Runs main loop to call runCycle. Closes reader and writer after an escape command is registered in runCycle.
+     * Running open
+     *
+     * @param reader ConsoleReader that handles IO reads
+     * @param writer ConsoleWriter that handles IO writes
      */
     public void open(ConsoleReader reader, ConsoleWriter writer) {
-        boolean close = false;
-        do {
-            if (!reader.ready() || !writer.ready()) {
-                break;
+        boolean canRun = reader.ready() && writer.ready() && this.context.getDefault() != null;
+        while (canRun) {
+            try {
+                canRun = this.runCycle(reader, writer);
+            } catch (InvalidContextException ex) {
+                System.err.println(ex.getMessage());
+            } catch (IOException ioEx) {
+                ioEx.printStackTrace();
             }
-            if (this.getActive() == null) {
-                this.setActive(this.main);
-            }
-            do {
-                this.sendEvent(new Event(EventType.PRE_RENDER), this.getActive());
-                this.awaitOutput(writer, this.getActive().getTextDisplay());
-                this.sendEvent(new Event(EventType.RENDER), this.getActive());
-                this.sendEvent(new Event(EventType.POST_RENDER), this.getActive());
-
-                String input = this.awaitInput(reader);
-                if (input == null || reader.getEscapeCommands().contains(input)) {
-                    close = true;
-                    break;
-                }
-
-                if (reader.getBackCommands().contains(input)) {
-                    this.context.pop();
-                } else {
-                    // Split the input, as it may have arguments and would not match a menu option
-                    String[] split = input.split("\\s+");
-                    MenuItem selected = this.getActive().getChild(split[0]);
-                    if (selected != null && !selected.isDisabled()) {
-                        this.sendEvent(new Event(EventType.EXECUTE, input), selected);
-                    }
-                }
-            } while (!this.context.refresh());
-        } while (!close);
-
+        }
         try {
             reader.close();
             writer.close();
